075690c7047fc598f79a0e6c48777141c74fc585
ScottDonovan <spdonovan@hotmail.co.uk>
2022-03-28 16:51:17 +0100

    Working project commited

---
+++ app/src/main/java/com/example/jetpackfragments/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.jetpackfragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+val STAR_SIGN_ID = "STAR_SIGN_ID"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
